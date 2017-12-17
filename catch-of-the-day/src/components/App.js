d656a2b18c66c64393849696d77679d80344bae4
Bruno Alla <bruno.alla@founders4schools.org.uk>
2017-12-17 22:35:10 +0000

    13: State

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,26 @@ import Order from './Order';
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    // initial state
+    this.state = {
+      fishes: {},
+      order: {},
+    }
+  }
+
+  addFish(fish) {
+    // Copy existing state
+    const fishes = {...this.state.fishes};
+    // Update it
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // Set the new state
+    this.setState({ fishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +32,7 @@ class App extends React.Component {
           <Header tagline="The freshest fishes in the area"/>
         </div>
         <Order/>
-        <Inventory/>
+        <Inventory addFish={ this.addFish } />
       </div>
     )
   }
