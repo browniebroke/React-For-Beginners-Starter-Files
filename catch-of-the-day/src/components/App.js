81782040ab72c8c46ce53594eb999f95398e139d
Bruno Alla <bruno.alla@founders4schools.org.uk>
2017-12-18 23:23:19 +0000

    Show fishes in menu

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from "./Inventory";
 import fishSamples from '../sample-fishes';
+import Fish from "./Fish";
 
 class App extends React.Component {
   constructor() {
@@ -36,6 +37,13 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="The freshest fishes in the area"/>
+          <ul className="list-of-fishes">
+            {
+              Object
+                .keys(this.state.fishes)
+                .map(key => <Fish key={ key } details={ this.state.fishes[key] }/>)
+            }
+          </ul>
         </div>
         <Order/>
         <Inventory addFish={ this.addFish } loadSamples={ this.loadSamples }/>
