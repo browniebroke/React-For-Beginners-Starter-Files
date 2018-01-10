f3f62c3cadadd75dd8ef8eb0c6341ade4e06e182
Bruno Alla <bruno.alla@founders4schools.org.uk>
2018-01-10 10:52:47 +0000

    Remove from order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -12,8 +12,10 @@ class App extends React.Component {
     super();
     this.addFish = this.addFish.bind(this);
     this.updateFish = this.updateFish.bind(this);
+    this.removeFish = this.removeFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.removeFromOrder = this.removeFromOrder.bind(this);
     // initial state
     this.state = {
       fishes: {},
@@ -61,6 +63,12 @@ class App extends React.Component {
     this.setState({ fishes })
   }
 
+  removeFish(key) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = null;
+    this.setState({ fishes })
+  }
+
   loadSamples() {
     this.setState({fishes: fishSamples});
   }
@@ -72,6 +80,15 @@ class App extends React.Component {
     order[key] = order[key] + 1 || 1;
     // Set State
     this.setState({order})
+  }
+
+  removeFromOrder(key) {
+    // Copy state
+    const order = {...this.state.order};
+    // Remove
+    delete order[key];
+    // Set State
+    this.setState({order});
   }
 
   render() {
@@ -93,10 +110,12 @@ class App extends React.Component {
         <Order
           fishes={this.state.fishes}
           order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
         />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
+          removeFish={this.removeFish}
           loadSamples={this.loadSamples}
           fishes={this.state.fishes}
         />
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -31,6 +31,7 @@ class Inventory extends React.Component {
         </select>
         <textarea name="desc" value={fish.desc} placeholder="Fish desc" onChange={(e) => this.handleChange(e, key)}/>
         <input type="text" value={fish.image} name="image" placeholder="Fish image" onChange={(e) => this.handleChange(e, key)}/>
+        <button onClick={() => this.props.removeFish(key)}>Remove Fish</button>
       </div>
     )
   }
